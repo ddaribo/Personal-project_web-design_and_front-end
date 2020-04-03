d2b8949391dc6b03d24584ebc358b420d6bbe597
Bozhidara Pachilova <bozhidara.p@abv.bg>
2020-04-03 13:15:06 +0300

    Langing page carousel is now responsive

---
+++ carousel-links.js
@@ -0,0 +1,20 @@
+if (window.location.search == '?collapseGDelchev') {
+  $('#collapseGDelchev').addClass('show');
+  window.scroll(0,490);
+}
+
+
+if (window.location.search == '?collapseBottomSt') {
+  $('#collapseBottomSt').addClass('show');
+  window.scroll(0,290);
+}
+
+if (window.location.search == '?collapseKostal') {
+  $('#collapseKostal').addClass('show');
+  window.scroll(0,750);
+}
+
+if (window.location.search == '?collapseChair') {
+  $('#collapseChair').addClass('show');
+  window.scroll(0,1110);
+}
