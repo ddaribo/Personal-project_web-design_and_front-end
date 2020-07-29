4597ae461fcb755bb3e0322461326d5fc05c8bd6
Bozhidara Pachilova <bozhidara.p@abv.bg>
2020-07-29 13:12:19 +0300

    Added meta tags

---
+++ README.md
@@ -0,0 +1,9 @@
+# brod-engineering
+A website I am making for a small business.
+
+Main project activites were:
+- web design & HTML&CSS implementation
+- media queries for responsiveness
+- fixing some specific browser issues (ex. Safari)
+- hosting on a platform with own domain
+- SEO optimisation & Google indexing request
--- form.php
+++ form.php
@@ -9,20 +9,18 @@ if(empty($_POST['name'])  ||
     $errors .= "\n Error: all fields are required";
 }
 
-echo $_POST['name'];
 
 if (isset($_POST['name'])) {
-    $name = $_POST['name'];
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
 }
 
 if (isset($_POST['emailForm'])) {
-    $email_address = $_POST['emailForm']; 
+    $email_address = filter_var($_POST['emailForm'], FILTER_SANITIZE_STRING); 
 }
 
 if (isset($_POST['message'])) {
-    $message = $_POST['message']; 
+    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING); 
 }
-
 
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
@@ -41,7 +39,7 @@ if( empty($errors))
 
     $email_subject = "Contact form submission: $name";
 
-    $email_body = "You have received a new message. ".
+    $email_body = "You have received a new message from brodengineering.eu. ".
 
     " Here are the details:\n Name: $name \n ".
     
@@ -56,10 +54,8 @@ if( empty($errors))
 
     mail($to,$email_subject,$email_body,$headers);
 
-    header('Location: bg/index.html');
+    header('Location: index.html');
 
 }
-
-    
 
 ?>
